2f59643c7235e10b1341e1d8419e9ebf46cdff68
Elhama Tokhi <tokhielhama30@gmail.com>
2025-09-17 00:58:45 +0200

    debug

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,15 +1,14 @@
 import { Pool } from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD ? "[HIDDEN]" : "MISSING");
+if (process.env.NODE_ENV !== "production") {
+  import("dotenv").then((dotenv) => dotenv.config());
+}
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
 });
+
 export default pool;
